c115e7f6aabf6965ce7e6b227bb3f3789b94650e
Bram Van der Sype <bram.vandersype@gmail.com>
2020-05-29 09:19:54 +0200

    Implement Country and VatNumber logic to valid purely the format of a vat number

---
+++ src/Country.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Antenna\Vat;
+
+use DragonBe\Vies\Validator;
+use InvalidArgumentException;
+use function array_key_exists;
+use function sprintf;
+
+final class Country
+{
+    private const COUNTRIES = [
+        'AT' => Validator\ValidatorAT::class,
+        'BE' => Validator\ValidatorBE::class,
+        'BG' => Validator\ValidatorBG::class,
+        'CY' => Validator\ValidatorCY::class,
+        'CZ' => Validator\ValidatorCZ::class,
+        'DE' => Validator\ValidatorDE::class,
+        'DK' => Validator\ValidatorDK::class,
+        'EE' => Validator\ValidatorEE::class,
+        'EL' => Validator\ValidatorEL::class,
+        'ES' => Validator\ValidatorES::class,
+        'FI' => Validator\ValidatorFI::class,
+        'FR' => Validator\ValidatorFR::class,
+        'HR' => Validator\ValidatorHR::class,
+        'HU' => Validator\ValidatorHU::class,
+        'IE' => Validator\ValidatorIE::class,
+        'IT' => Validator\ValidatorIT::class,
+        'LU' => Validator\ValidatorLU::class,
+        'LV' => Validator\ValidatorLV::class,
+        'LT' => Validator\ValidatorLT::class,
+        'MT' => Validator\ValidatorMT::class,
+        'NL' => Validator\ValidatorNL::class,
+        'PL' => Validator\ValidatorPL::class,
+        'PT' => Validator\ValidatorPT::class,
+        'RO' => Validator\ValidatorRO::class,
+        'SE' => Validator\ValidatorSE::class,
+        'SI' => Validator\ValidatorSI::class,
+        'SK' => Validator\ValidatorSK::class,
+        'GB' => Validator\ValidatorGB::class,
+    ];
+
+    private string $code;
+
+    private function __construct(string $code)
+    {
+        $this->code = $code;
+    }
+
+    public static function fromCode(string $code) : self
+    {
+        if (array_key_exists($code, self::COUNTRIES)) {
+            return new self($code);
+        }
+
+        throw new InvalidArgumentException(sprintf('Code "%s" is not a valid country code.', $code));
+    }
+
+    public function code() : string
+    {
+        return $this->code;
+    }
+
+    public function validator() : Validator\ValidatorInterface
+    {
+        return new (self::COUNTRIES[$this->code])();
+    }
+}
--- src/VatNumber.php
+++ src/VatNumber.php
@@ -4,6 +4,46 @@ declare(strict_types=1);
 
 namespace Antenna\Vat;
 
+use InvalidArgumentException;
+use RuntimeException;
+use function preg_replace;
+use function sprintf;
+use function substr;
+
 final class VatNumber
 {
+    private string $vatNumber;
+
+    public function __construct(string $vatNumber)
+    {
+        $this->vatNumber = $vatNumber;
+    }
+
+    public function country() : Country
+    {
+        $countryCode = substr($this->stripped(), 0, 2);
+
+        try {
+            return Country::fromCode($countryCode);
+        } catch (InvalidArgumentException $e) {
+            throw new RuntimeException(
+                sprintf('Unable to determine valid country for vat number "%s"', $this->vatNumber)
+            );
+        }
+    }
+
+    public function number() : string
+    {
+        return substr($this->stripped(), 2);
+    }
+
+    public function stripped() : string
+    {
+        return preg_replace('/[^a-zA-Z0-9]+/', '', $this->vatNumber);
+    }
+
+    public function isFormatValid() : bool
+    {
+        return $this->country()->validator()->validate($this->number());
+    }
 }
